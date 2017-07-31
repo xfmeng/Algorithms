00bee13a814921b057bf80a3d28e7627b936fbf6
xfmeng <1075857522@qq.com>
2017-07-31 16:18:23 +0800

    提交文件

---
+++ chapter1/E1_1_3.java
@@ -0,0 +1,20 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class E1_1_3 {
+	
+	public boolean equals(int a, int b, int c)
+	{
+		return(a == b && b == c && a == c);
+	}
+	
+	public static void main(String[] args) {
+		E1_1_3 exercise = new E1_1_3();
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		System.out.println(exercise.equals(a, b, c));
+	}
+}
